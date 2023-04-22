e515e95b13b6d7444e132fd52014c94fd1c67560
Nitish Nashine <nitishnashine@gmail.com>
2023-04-22 14:44:52 +0530

    Without modify Array

--- src/Array/findDuplicateNo.java
+++ src/Array/findDuplicateNo.java
@@ -13,9 +13,21 @@ public class findDuplicateNo {
         }
         return -1;
     }
+
+    public static int findDuplicateWithoutModifying(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n+1];
+        for (int i = 0; i < n; i++){
+            arr[nums[i]]++;
+            if (arr[nums[i]] > 1){
+                return nums[i];
+            }
+        }
+        return n;
+    }
     public static void main(String[] args) {
         int[] nums = {1,3,4,2,2};
-        int ans = findDuplicate(nums);
+        int ans = findDuplicateWithoutModifying(nums);
         System.out.println(ans);
     }
 }
